73d1848bd53f94ec5b32ed821070be6cf6989299
Putra Krishna <krishna.putra332@gmail.com>
2021-08-16 09:04:16 +0700

    ziunadev: can i contribute?

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,11 +1,20 @@
+/**
+ * add some dependencies
+ */
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import './css/navbar.css'
 
-export const Navbar = () => {
+export const Navbar = ({scrolled}) => { // getting props from App.js for navbar styling logic
+  
     return (
-      <nav>
-         <main>
-         <h1>Quran App</h1>
-         </main>
+      <nav className={`nav ${scrolled && 'scrolled-nav'}`}> {/* css ternary for dynamically calling class */}
+         <div>
+         {/* <h1>Quran App</h1> */}
+          <Link to='/'> {/* Make the title clickable and directly switch to home if clicked */}
+            <h1>Quran App</h1> {/* wrapping to make it more editable */}
+          </Link>
+         </div>
       </nav>
     )
 }
